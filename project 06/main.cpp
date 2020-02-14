03cd0a6ee47c214f5808b76ac59c1acb3c125dd2
Aaron916 <ar18002>
2020-02-13 19:30:31 -0800

    Initial Commit

---
+++ project 06/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project 06
+//
+//  Created by Aaron Rooks on 2/13/20.
+//  Copyright © 2020 Aaron Rooks. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
