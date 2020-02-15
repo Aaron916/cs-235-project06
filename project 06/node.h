8677a1663e45c929971a5c339aa80abd5841b7fe
AtomicGothic <atomicgothic@protonmail.com>
2020-02-15 10:46:06 -0500

    Changed several parts of the code to get it to compile. Was throwing bad_alloc exceptions. See -AG notes.

--- project 06/node.h
+++ project 06/node.h
@@ -10,29 +10,20 @@ public:
 	Node<T>* pNext;
 	Node<T>* pPrev;
 
-	Node()
-	{
-		pNext = NULL;
-		pPrev = NULL;
-	}
+	Node() : pNext(NULL), pPrev(NULL), data() {}
 
-	Node(T t)
-	{
-		data = t;
-		pNext = NULL;
-		pPrev = NULL;
-	}
-
+	// non-default. Initialize data as we create the node
+	Node(const T& data) : pNext(NULL), pPrev(NULL), data(data) {} // changed the format of the constructors to be in line with what the instructor has. -AG
 };
 
 template <class T>
 Node <T>* copy(Node<T>* pSource) // removed the const before the argument to test a fix for an error.
 {
-	Node<T> *pDestination = new Node<T>(pSource->data);
-    Node<T> *pSrc = pSource;
-	Node<T> *pDes = pDestination;
+	Node<T>* pDestination = new Node<T>(pSource->data);
+	Node<T>* pSrc = pSource;
+	Node<T>* pDes = pDestination;
 
-	while (pSrc->pNext)
+	for (Node<T>* p = pSource; p != NULL; p = p->pNext) // Had to change the loop. It wasn't copying correctly. It was throwing bad_alloc exceptions. -AG
 	{
 		pDes = insert(pDes, pSrc->data, true);
 	}
@@ -40,9 +31,9 @@ Node <T>* copy(Node<T>* pSource) // removed the const before the argument to tes
 }
 
 template <class T>
-Node <T>* insert(Node<T> *pCurrent, const T &t, bool after = false)
+Node <T>* insert(Node<T>*& pCurrent, const T& t, bool after = false)
 {
-	Node<T>* pNew = new Node<T>;
+	Node<T>* pNew = new Node<T>(t);
 
 	if (pCurrent && after == false)
 	{
@@ -72,31 +63,34 @@ Node <T>* insert(Node<T> *pCurrent, const T &t, bool after = false)
 template <class T>
 Node <T>* find(Node<T>* pHead, const T& t)
 {
-	while (pHead->pNext)
-	{
-		pHead->data = t;
-		return pHead;
-	}
+	// Changed the contents of this function to what the instructor posted in the discussion board. -AG
+	// loop through the linked list
+	for (Node <T>* p = pHead; p != NULL; p = p->pNext)
+		if (t == p->data)
+			return p;
+
 	return NULL;
 }
 
 template <class T>
-void freeData(Node<T>* & pHead)
+void freeData(Node<T>*& pHead)
 {
-	while (pHead)
+	// Changed the contents of this function to what the instructor posted in the discussion board. -AG
+	Node <T>* pNext;
+	for (Node <T>* p = pHead; p; p = pNext)
 	{
-		Node<T>* pDelete = pHead;
-		pHead = pHead->pNext;
-		delete pDelete;
+		pNext = p->pNext;
+		delete p;
 	}
+	pHead = NULL;
 }
 
 template <class T>
 Node <T>* remove(const Node<T>* pRemove)
 {
-    if (NULL == pRemove)
+	if (NULL == pRemove)
 		return NULL; // added NULL because we needed to return some value. //
-    
+
 	Node<T>* pReturn = NULL; // Initialized to NULL so that there was no case in which it could return an unititialized value //
 
 	if (pRemove->pPrev)
@@ -114,13 +108,48 @@ Node <T>* remove(const Node<T>* pRemove)
 	return pReturn;
 }
 
+//changed this operator overload method to what the instructor shared in the discussion board.
 template <class T>
-std::ostream& operator << (std::ostream& out, const Node<T>* list)
+std::ostream& operator << (std::ostream& out, const Node <T>* pHead)
 {
-	if (list)
-	{
-		out << list->data;
-	}
-	return out;
+	// paranoia
+	if (pHead == NULL)
+		return out;
+
+	// display the data
+	out << pHead->data;
+
+	// if there is something after us, display it
+	if (pHead->pNext)
+		return out << ", " << pHead->pNext;
+	else
+		return out;
 }
+
+/******************************************************
+* FIND SORTED
+* Find the spot in a given linked list where a given Node
+* is to go. We will return a pointer to the node preceeding
+* one where the new node will exist in the list
+* INPUT : a pointer to the head of the linked list
+* the value we are inserting
+* OUTPUT : a pointer to the node we will be inserting after
+* COST : O(n)
+********************************************************/
+template <class T> // Added this function from the instructor's post in the discussion board.
+Node <T>* findSorted(Node <T>* pHead,
+	const T& t)
+{
+	// trivial case
+	if (NULL == pHead || t < pHead->data)
+		return NULL;
+
+	// search for the node
+	while (pHead->pNext && pHead->pNext->data < t)
+		pHead = pHead->pNext;
+
+	return pHead;
+}
+
+
 #endif // NODE_H
